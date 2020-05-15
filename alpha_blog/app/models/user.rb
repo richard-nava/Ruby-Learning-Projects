2f9e29f898b5597ea5fc3f53d928178db6648e9a
thecleanx <59400191+thecleanx@users.noreply.github.com>
2020-05-15 10:36:06 -0500

    User validations

--- alpha_blog/app/models/user.rb
+++ alpha_blog/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
